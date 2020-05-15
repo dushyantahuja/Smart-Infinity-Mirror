521b686cff1209e85d1a14274225574b1d745fff
Dushyant Ahuja <dusht.ahuja@gmail.com>
2020-05-15 17:37:42 +0100

    Update Infinity_Mirror_v1.ino

--- Infinity_Mirror_v1/Infinity_Mirror_v1.ino
+++ Infinity_Mirror_v1/Infinity_Mirror_v1.ino
@@ -5,31 +5,44 @@
 #include <SerialCommand.h>
 #include "EEPROM.h"
 #include "TimerOne.h"
+// #include "OneButton.h"
 
 #define NUM_LEDS 60
-#define DATA_PIN 8
+#define DATA_PIN 5
 #define UPDATES_PER_SECOND 100
-#define SWITCHPIN 2
+// #define SWITCHPIN 2
 
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
+// Variables for IR Proximity
+
+// int IRpin = A1;               // IR photodiode on analog pin A1
+// int IRemitter = 13;            // IR emitter LED on digital pin 4
+
+//
+
+// CRGBPalette16 currentPalette;
+// TBlendType    currentBlending;
 
 CRGB leds[NUM_LEDS],minutes,hours,seconds,l,bg,lines;
 RTC_DS1307 rtc;
 SerialCommand sCmd;
+// OneButton button1(SWITCHPIN, true);
 boolean missed=0, ledState = 1, lastsec=1, multieffects = 0;
 byte lastsecond, rain;
 int light_low, light_high;
 DateTime now;
 
+void(* resetFunc) (void) = 0;
+
 void setup() {
+  // digitalWrite(IRemitter,LOW);             // turning the IR LEDs off - as a precaution - they don't have current limiting resistors
   Wire.begin();
   rtc.begin();
   Serial.begin(115200);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  currentPalette = PartyColors_p;
   // currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
+  // currentBlending = NOBLEND;
+  
+  // ******** Setup the default values for parameters (if not set before)
   if (EEPROM.read(99) != 1){               // Check if colours have been set or not
     EEPROM.write(0,255);                   // Seconds Colour - R-G-B - White
     EEPROM.write(1,255);
@@ -69,7 +82,7 @@ void setup() {
   
   // ********** Setup the serial commands
   
-  sCmd.addCommand("MULTI", set_multi);
+  //sCmd.addCommand("MULTI", set_multi);
   sCmd.addCommand("STAT", clockstatus);
   sCmd.addCommand("SETRAIN", set_rainbow);
   sCmd.addCommand("HOUR", set_hour);
@@ -77,51 +90,70 @@ void setup() {
   sCmd.addCommand("SEC", set_second);
   sCmd.addCommand("BG", set_bg);
   sCmd.addCommand("LIGHT", set_light);
-  sCmd.addCommand("RAINBOW", effects);
+  // sCmd.addCommand("RAINBOW", effects);
   sCmd.addCommand("TIME", set_time);
   // sCmd.addCommand("SETPARA", set_para);
-  sCmd.addCommand("MISSED", missedCall);
-  sCmd.addCommand("MISSEDOFF", missedOff);
-  sCmd.addCommand("RAINBOW", effects);
-  sCmd.addCommand("MISSED", missedCall);
-  sCmd.addCommand("MISSEDOFF", missedOff);
-  sCmd.setDefaultHandler(effects);
+  // sCmd.addCommand("MISSED", missedCall);
+  // sCmd.addCommand("MISSEDOFF", missedOff);
+  sCmd.addCommand("RESET", resetFunc);
+  // sCmd.addDefaultHandler(effects);
   
   // ********** Set all LEDs to background colour
-  fill_solid(leds, NUM_LEDS, bg);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = bg;
+  }
+  
+  // pinMode(IRemitter,OUTPUT);  // IR emitter LED on digital pin 2
+  // digitalWrite(IRemitter,LOW);// setup IR LED as off
   clockstatus();
+  // button1.attachClick(set_multi);
+  // attachInterrupt(1, set_multi, FALLING);
   Timer1.initialize();
   Timer1.attachInterrupt(state, 500000);
 }
 
 void loop() {
   sCmd.readSerial();
-  // fill_solid(leds, NUM_LEDS, bg);
-  if(multieffects){                 // Check if the button for multi-effects has been pressed
+  // button1.tick();
+  /* if(readIR(10) > 50){            // Switch off LEDs if someone is near the mirror - so that it can be used as a mirror. Switch off LEDs between 12:00 and 6:00 to save energy and cool down the LEDs and power supplies
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+    ledState = 1;
+    //FastLED.delay(200);
+  }
+  else { */
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = bg;
+  } 
+  /* if(multieffects){                 // Check if the button for multi-effects has been pressed
     uint8_t secondHand;
     secondHand = now.second();
+    if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = BLEND; }
+    // if( secondHand == 30)  { currentPalette = RainbowStripeColors_p;   currentBlending = BLEND;  }
+    // if( secondHand == 30)  { currentPalette = CloudColors_p;           currentBlending = BLEND; }
+    // if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = BLEND; }
     static uint8_t startIndex = 0;
     startIndex = startIndex + 1;
-    currentPalette = RainbowStripeColors_p;
-    currentBlending = LINEARBLEND;
     FillLEDsFromPaletteColors( startIndex);
     FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
   }
-  else 
+  else */
   if(ledState){                // Main clock code
-    // FastLED.delay(1000 / UPDATES_PER_SECOND);
-    int x = light_high;      
-    now = rtc.now();               //  DateTime(2014,5,2,22,20,0); //
-    if(( now.minute() % rain == 0 && now.second() == 0)){
+    // Setting brightness to light_high
+    int x = light_high; // analogRead(IRpin);
+    now =  rtc.now();        // DateTime(2014,5,2,22,20,0); //
+    /* if(( now.minute() % rain == 0 && now.second() == 0)){
        effects();
-    }
-    LEDS.setBrightness(constrain(light_low,0,100));
-    fill_solid(leds, NUM_LEDS, bg);
-    if(now.hour() < 6) LEDS.setBrightness(constrain(light_low,0,100)); // Set brightness to light_low during night - cools down LEDs and power supplies.
-    else LEDS.setBrightness(constrain(light_high,10,255));
+    }*/
     for(byte i=0; i<=now.minute();i++){
+      //Serial.println(minutes);
       leds[i] = minutes;
+    }
+    //Serial.println(now.hour(),DEC);
+    for(byte i = 0; i<60; i+=5){
+      leds[i]=CRGB::White;
     }
     leds[now.hour()%12*5]=hours;
     leds[now.hour()%12*5+1]=hours;
@@ -131,9 +163,8 @@ void loop() {
     /*for(byte i=(now.hour()%12)*5; i<=((now.hour())%12)*5+(now.minute()/12);i++){
       leds[i] = hours;
     }*/
-    for(byte i = 0; i<60; i+=5){
-      leds[i]=CRGB(50,50,50);
-    }
+    if(now.hour() < 6) LEDS.setBrightness(constrain(light_low,0,100)); // Set brightness to light_low during night - cools down LEDs and power supplies.
+    else LEDS.setBrightness(constrain(light_high,10,255));
     if(lastsec){
       l=leds[now.second()];
       leds[now.second()] = seconds;
@@ -142,39 +173,43 @@ void loop() {
       // Serial.println("ON");
     } else {
       leds[lastsecond] = l;
-      // if(shuttle()) all_off();
+      if(missed) all_off();
       // Serial.println("OFF");
       lastsec = 1;
     }
     FastLED.show();
-    // show_at_max_brightness_for_power();
     ledState = 0;
-   }
+  }
+  //delay(250);
+  // if(multieffects) FastLED.delay(1000 / UPDATES_PER_SECOND);
+  // }
 } 
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-  uint8_t brightness = light_high;
+  /* ,uint8_t brightness = 150;
   
   for( int i = 0; i < NUM_LEDS; i++) {
     leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
     colorIndex += 3;
-  }
+  }*/
 }
 
-void set_multi(){
+/* void set_multi(){
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 200) 
   {
     if(multieffects){
-      fill_solid(leds, NUM_LEDS, bg);
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = bg;
+      }
     }
     multieffects = !multieffects;
     Serial.println(multieffects);
   }
   last_interrupt_time = interrupt_time;
-}
+}*/
 
 void set_rainbow(){
   rain = atoi(sCmd.next());
@@ -208,6 +243,8 @@ void clockstatus(){
   Serial.print(hours.g);
   Serial.print(" ");
   Serial.println(hours.b);
+  // Serial.print("Ambient Light: ");
+  // Serial.println(analogRead(IRpin));
   Serial.print("Light set - High:");
   Serial.println(light_high,DEC);
   Serial.print("Light set - Low:");
@@ -226,6 +263,8 @@ void clockstatus(){
   Serial.print(':');
   Serial.print(now.second(), DEC);
   Serial.println();
+  // Serial.print("Distance: ");
+  // Serial.println(readIR(5),DEC);
 }
 
 void state(){
@@ -234,31 +273,20 @@ void state(){
 
 const int colorWheelAngle = 255 / NUM_LEDS;
 
-void effects(){
+/* void effects(){
   Serial.println("RAINBOW");
-  for( int j = 0; j< 300; j++){
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-  FastLED.show();
-  FastLED.delay(1000/UPDATES_PER_SECOND);
-  }
-  /*for (int j=0; j<3; j++){
+  for (int j=0; j<3; j++){
     for (int i = 0; i < 60; i++) { 
       FillLEDsFromPaletteColors(i);
       FastLED.show();
-      // show_at_max_brightness_for_power();
-      // delay_at_max_brightness_for_power( 30);
       delay(30);
     }
-  }*/
+  }
   lastsec = 1;
-}
+  // FastLED.show();
+}*/
 
-void missedCall()
+/* void missedCall()
 {
     missed = 1;
 }
@@ -267,9 +295,12 @@ void missedOff()
 {
     missed = 0;
 }
+*/
 
 void all_off(){
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
 }
 
 void set_hour(){
@@ -310,7 +341,9 @@ void set_bg(){
   EEPROM.write(10,bg.g);
   EEPROM.write(11,bg.b);
   Serial.println("BG COLOUR SET");
-  fill_solid(leds, NUM_LEDS, bg);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = bg;
+  }  
 }
 
 void set_light(){
@@ -323,12 +356,47 @@ void set_light(){
 
 void set_time(){
   DateTime now = rtc.now();
-  Serial.println("TIME SET");
+  Serial.println("IN SETTIME");
   uint8_t hr = (uint8_t)atoi(sCmd.next());
   uint8_t minu = (uint8_t)atoi(sCmd.next());
   uint8_t sec = (uint8_t)atoi(sCmd.next());
-  // Serial.println(hr);
-  // Serial.println(minu);
-  // Serial.println(sec);
+  Serial.println(hr);
+  Serial.println(minu);
+  Serial.println(sec);
   rtc.adjust(DateTime(now.year(),now.month(),now.day(),hr,minu,sec));
 }
+  
+/* int readIR(int times){
+  int ambientIR;                              // variable to store the IR coming from the ambient
+  int obstacleIR;                             // variable to store the IR coming from the object
+  int value[10];                              // variable to store the IR values
+  int distance;                               // variable that will tell if there is an obstacle or not
+  for(int x=0;x<times;x++){     
+    digitalWrite(IRemitter,LOW);              // turning the IR LEDs off to read the IR coming from the ambient
+    delay(1);                                             // minimum delay necessary to read values
+    ambientIR = analogRead(IRpin);            // storing IR coming from the ambient
+    digitalWrite(IRemitter,HIGH);             // turning the IR LEDs on to read the IR coming from the obstacle
+    delay(1);                                 // minimum delay necessary to read values
+    obstacleIR = analogRead(IRpin);           // storing IR coming from the obstacle
+    value[x] = ambientIR-obstacleIR;          // calculating changes in IR values and storing it for future average
+  }
+ 
+  for(int x=0;x<times;x++){                   // calculating the average based on the "accuracy"
+    distance+=value[x];
+  }
+  digitalWrite(IRemitter,LOW);                // turning the IR LEDs off
+  return(distance/times);                     // return the final value
+  return 0;
+} */
+
+/* void set_para(){
+  String para = (String)sCmd.next();
+  if(para == "SETRAIN") set_rainbow(); 
+  else if( para == "HOUR") set_hour();
+  else if( para == "MIN") set_minute();
+  else if( para == "SEC") set_second();
+  else if( para == "BG") set_bg();
+  else if( para == "LIGHT") set_light();
+  else if( para == "TIME") set_time();
+}
+*/
